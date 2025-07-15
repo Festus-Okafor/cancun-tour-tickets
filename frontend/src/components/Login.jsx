d0f497c940037bed8a1be321625c48d0ba381d6b
mexie07 <okaforfestus07@gmail.com>
2025-07-15 18:50:15 -0400

    working on frontend

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+ 
+        <h2>Welcome Back</h2>
+        <p>The time to relax is when you dont have time for it</p>
+   
+    </>
+  )
+}
+
+export default Login
